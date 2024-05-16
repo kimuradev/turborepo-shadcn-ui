104ca6c7543c2a3bcd5b650bbdfc620123958e22
Leandro Kimura <kimuradev@gmail.com>
2024-05-16 14:43:44 -0300

    adjust game schedule style

--- apps/tennis/app/dashboard/game-scheduled/game-table.tsx
+++ apps/tennis/app/dashboard/game-scheduled/game-table.tsx
@@ -12,7 +12,7 @@ export default function GameTable({ data }: GameTable) {
         <div className="overflow-auto max-h-[400px] w-full">
             {data.map((game: any) => {
                 return (
-                    <div>
+                    <div className="flex flex-col gap-1">
                         <div className="bg-secondary font-bold px-2 py-1 rounded">{format(parseISO(game.scheduleDate), 'dd/MM/yyyy - eee', { locale: ptBR })}</div>
                         {game.games.map((item: any) => (
                             <div className="text-sm px-4 py-1">{format(item.schedule, 'HH:mm')} - {getNameWithAbbreviation(item.player1?.name)} x {getNameWithAbbreviation(item.player2?.name)}</div>
