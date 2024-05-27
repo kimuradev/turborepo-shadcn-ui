a7998fe1aaf11c3c964b1a6bcf790902bdbeecfb
Leandro Kimura <kimuradev@gmail.com>
2024-05-27 13:27:07 -0300

    add hour timezone function

--- apps/tennis/app/dashboard/game-scheduled/game-table.tsx
+++ apps/tennis/app/dashboard/game-scheduled/game-table.tsx
@@ -1,6 +1,6 @@
 import { getFormattedDate, getNameWithAbbreviation } from "@/lib/utils";
 // import { format } from "date-fns";
-import { format } from 'date-fns-tz';
+import { format, toZonedTime } from 'date-fns-tz'
 import ShareButton from "./share-button";
 import { CalendarX } from "lucide-react";
 import { DetailsProps } from "@/lib/definitions";
@@ -24,19 +24,27 @@ export default function GameTable({ data }: GameTable) {
         )
     }
 
+    const formatHourTimezone = (dateStr: string) => {
+        // Convert the date to the desired timezone
+        const timeZone = 'America/Sao_Paulo';
+        const zonedDate = toZonedTime(dateStr, timeZone);
+        // Format the date to HH:mm in the specified timezone
+        return format(zonedDate, 'HH:mm', { timeZone });
+    }
 
     return (
         <div className="overflow-auto max-h-[400px] w-full">
             <ShareButton data={data} />
             {data.map((game: any) => {
                 return (
-                    <div className="flex flex-col gap-1">
+                    <div className="flex flex-col gap-1" key={game.id}>
                         <div className="bg-secondary font-bold px-2 py-1 rounded">{getFormattedDate(game.scheduleDate)}</div>
-                        {game.games.map((item: { schedule: string, player1: { name: string }, player2: { name: string } }) => (
-                            <div className="text-sm px-4 py-1">{format(item.schedule, 'HH:mm', {
-                                timeZone: 'America/Sao_Paulo',
-                            })} - {getNameWithAbbreviation(item.player1?.name)} x {getNameWithAbbreviation(item.player2?.name)}</div>
-                        ))}
+                        {game.games.map((item: { schedule: string, player1: { name: string }, player2: { name: string } }) => {
+                            return (
+                                <div className="text-sm px-4 py-1" key={item.schedule}>{formatHourTimezone(item.schedule)} - {getNameWithAbbreviation(item.player1?.name)} x {getNameWithAbbreviation(item.player2?.name)}</div>
+                            )
+                        }
+                        )}
                     </div>
                 )
             })}
