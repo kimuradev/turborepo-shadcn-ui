06c5772960edf5462467d8d6a8bc2c1e7b9ecd44
Leandro Kimura <kimuradev@gmail.com>
2024-06-17 08:37:17 -0300

    remove doubles schedule

--- apps/tennis/app/dashboard/game-scheduled/game-table.tsx
+++ apps/tennis/app/dashboard/game-scheduled/game-table.tsx
@@ -1,10 +1,4 @@
 import { getFormattedDate, getNameWithAbbreviation } from "@/lib/utils";
-// import { format } from "date-fns";
-import isArray from 'lodash/isArray';
-import cloneDeep from 'lodash/cloneDeep';
-import findIndex from 'lodash/findIndex';
-import mergeWith from 'lodash/mergeWith';
-import sortBy from 'lodash/sortBy';
 import { format, toZonedTime } from 'date-fns-tz'
 import ShareButton from "./share-button";
 import { CalendarX } from "lucide-react";
@@ -20,117 +14,14 @@ type GameTable = {
 }
 
 export default function GameTable({ data }: GameTable) {
-    // if (!data.length) {
-    //     return (
-    //         <div className="flex flex-col justify-center items-center mt-6">
-    //             <CalendarX className="w-[50px] h-[50px] stroke-primary mb-4" />
-    //             <p className="text-sm text-muted-foreground">Nenhum jogo marcado no momento. </p>
-    //         </div>
-    //     )
-    // }
-
-    // TODO: REMOVE IT AFTER DUPLAS TOURNAMENTS
-    const newData = [
-    //     { scheduleDate: '2024-06-11', games: [
-    //         {
-    //             id: 1,
-    //             game_id: 1,
-    //             schedule: '2024-06-11T21:30:00.000Z',
-    //             player1: { name: 'Kimura/Clebis' },
-    //             player2: { name: 'Jose/Marco' },
-    //         },
-    //         {
-    //             id: 2,
-    //             game_id: 2,
-    //             schedule: '2024-06-11T22:30:00.000Z',
-    //             player1: { name: 'Perdedor 1' },
-    //             player2: { name: 'Nilton/Karin' },
-    //         },
-    //         {
-    //             id: 3,
-    //             game_id: 3,
-    //             schedule: '2024-06-12T23:30:00.000Z',
-    //             player1: { name: 'Nilton/Karin' },
-    //             player2: { name: 'Vencedor 1' },
-    //         },
-    //     ]
-    // },
-    {
-        scheduleDate: '2024-06-13', games: [
-            {
-                id: 1,
-                game_id: 1,
-                schedule: '2024-06-13T21:30:00.000Z',
-                player1: { name: 'Navarro/Susin' },
-                player2: { name: 'Vlademir/Floriano' },
-            },
-            {
-                id: 2,
-                game_id: 2,
-                schedule: '2024-06-13T22:30:00.000Z',
-                player1: { name: 'Perdedor 1' },
-                player2: { name: 'Adriano/Marcelo' },
-            },
-            {
-                id: 3,
-                game_id: 3,
-                schedule: '2024-06-13T23:30:00.000Z',
-                player1: { name: 'Adriano/Marcelo' },
-                player2: { name: 'Vencedor 1' },
-            },
-        ]
-    },
-    {
-        scheduleDate: '2024-06-14', games: [
-            {
-                id: 1,
-                game_id: 1,
-                schedule: '2024-06-14T21:30:00.000Z',
-                player1: { name: 'Robinson/Claudinei' },
-                player2: { name: 'Angelo/Makoto' },
-            },
-            {
-                id: 2,
-                game_id: 2,
-                schedule: '2024-06-14T22:30:00.000Z',
-                player1: { name: 'Perdedor 1' },
-                player2: { name: 'Tiago/Rogerinho' },
-            },
-            {
-                id: 3,
-                game_id: 3,
-                schedule: '2024-06-14T23:30:00.000Z',
-                player1: { name: 'Tiago/Rogerinho' },
-                player2: { name: 'Vencedor 1' },
-            },
-        ]
-    },
-    {
-        scheduleDate: '2024-06-15', games: [
-            {
-                id: 1,
-                game_id: 1,
-                schedule: '2024-06-15T11:00:00.000Z',
-                player1: { name: '1º primeiro' },
-                player2: { name: '1º segundo' },
-            },
-            {
-                id: 2,
-                game_id: 2,
-                schedule: '2024-06-15T11:00:00.000Z',
-                player1: { name: '2º primeiro' },
-                player2: { name: '3º primeiro' },
-            },
-            {
-                id: 3,
-                game_id: 3,
-                schedule: '2024-06-15T12:30:00.000Z',
-                player1: { name: 'Vencedor semi-1' },
-                player2: { name: 'Vencedor semi-2' },
-            },
-        ]
-    },
-    ]
+    if (!data.length) {
+        return (
+            <div className="flex flex-col justify-center items-center mt-6">
+                <CalendarX className="w-[50px] h-[50px] stroke-primary mb-4" />
+                <p className="text-sm text-muted-foreground">Nenhum jogo marcado no momento. </p>
+            </div>
+        )
+    }
 
     const formatHourTimezone = (dateStr: string) => {
         // Convert the date to the desired timezone
@@ -140,28 +31,10 @@ export default function GameTable({ data }: GameTable) {
         return format(zonedDate, 'HH:mm', { timeZone });
     }
 
-    function customizer(objValue: any, srcValue: any) {
-        if (isArray(objValue)) {
-            return objValue.concat(srcValue);
-        }
-    }
-
-    const mergedData = cloneDeep(data);  // Create a deep clone of data to avoid modifying the original
-    newData.forEach((newItem: any) => {
-        const index = findIndex(mergedData, { scheduleDate: newItem.scheduleDate });
-        if (index !== -1) {
-            mergeWith(mergedData[index], newItem, customizer);
-        } else {
-            mergedData.push(newItem);
-        }
-    });
-    // Sort the mergedData array by scheduleDate
-    const sortedData = sortBy(mergedData, ['scheduleDate']);
-
     return (
         <div className="overflow-auto max-h-[400px] w-full">
-            <ShareButton data={sortedData} />
-            {sortedData.map((game: any) => {
+            <ShareButton data={data} />
+            {data.map((game: any) => {
                 return (
                     <div className="flex flex-col gap-1" key={game.id}>
                         <div className="bg-secondary font-bold px-2 py-1 rounded">{getFormattedDate(game.scheduleDate)}</div>
