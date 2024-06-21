d0320c7ab09e1ecaf2cf488576ac4ec9a4e3fe77
Leandro Kimura <kimuradev@gmail.com>
2024-06-21 15:00:36 -0300

    enable delete user only without checked

--- apps/tennis/app/admin/users/management/table/useUserTable.tsx
+++ apps/tennis/app/admin/users/management/table/useUserTable.tsx
@@ -81,7 +81,7 @@ export function useUserTable({ setDialog }: { setDialog : any }) {
           <>
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
-                {signed && isAdmin && (
+                {signed && isAdmin && !row.getValue("checked") && (
                   <Button variant="ghost" className="h-8 w-8 p-0">
                     <span className="sr-only">Abrir menu</span>
                     <MoreHorizontal className="h-4 w-4" />
